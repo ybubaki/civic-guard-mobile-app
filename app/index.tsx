7f7982cf1974a454615de48cdb0601db496828f1
buks <ybubaki@gmail.com>
2025-05-25 15:37:40 +0000

    feat: Nativewind Integrated.

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,18 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text className="text-2xl">
+          Edit app/index.tsx to edit this screen.
+        </Text>
+      </View>
+    </>
   );
 }
