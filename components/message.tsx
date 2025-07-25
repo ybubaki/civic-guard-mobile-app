6548e2d090817fc00f383bab5335c78b0dd684d6
buks <ybubaki@gmail.com>
2025-07-25 18:34:46 +0000

    modification for help center.

--- components/message.tsx
+++ components/message.tsx
@@ -1,3 +1,4 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { View, Text } from "react-native";
 
 interface MessageItemProps {
@@ -7,14 +8,24 @@ interface MessageItemProps {
 
 const MessageItem: React.FC<MessageItemProps> = ({ message, type }) => {
   return (
-    <View
-      className="max-w-[80%] rounded-xl p-2 mb-4"
-      style={{
-        alignSelf: type === "ai" ? "flex-start" : "flex-end",
-        backgroundColor: type === "ai" ? "#dcfce7" : "#f3f4f6",
-      }}
-    >
-      <Text className="text-base">{message}</Text>
+    <View className={type === "ai" ? "flex-row items-start gap-2" : ""}>
+      {type === "ai" && (
+        <MaterialCommunityIcons
+          name="robot-outline"
+          size={24}
+          color="black"
+          className="mt-2"
+        />
+      )}
+      <View
+        className="max-w-[80%] rounded-xl p-2 mb-4 bg-green-50"
+        style={{
+          alignSelf: type === "ai" ? "flex-start" : "flex-end",
+          backgroundColor: type === "ai" ? "#f0fdf4" : "#f3f4f6",
+        }}
+      >
+        <Text className="text-base">{message}</Text>
+      </View>
     </View>
   );
 };
--- service/index.ts
+++ service/index.ts
@@ -1,4 +1,4 @@
-const ROOT_URL = "http://172.20.10.3:5000";
+const ROOT_URL = "http://192.168.6.59:5000";
 
 export const API_BASE_URL = `${ROOT_URL}/api`;
 export const BASE_URL = ROOT_URL;
